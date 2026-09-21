caf7c33c14bf0704670268c22259f5bde0b875f8
Paveshen Padayachee <istimadan@gmail.com>
2026-09-21 07:05:00 +0200

    finalise unit tests and code

--- credit-card-transactions/ml-net-example/ML.NET.Example/ML.NET.API.Tests/Controllers/TransactionsControllerTests.cs
+++ credit-card-transactions/ml-net-example/ML.NET.Example/ML.NET.API.Tests/Controllers/TransactionsControllerTests.cs
@@ -1,14 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.AspNetCore.Mvc;
+using ML.NET.API.Controllers;
+using ML.NET.API.Services;
+using ML.NET.API.Validators;
+using Moq;
 
 namespace ML.NET.API.Tests.Controllers;
 
 public class TransactionsControllerTests
 {
-    [Fact]
-    public void Test1()
-    {
+    private readonly CardTransactionValidator validator = new();
+    private readonly Mock<IPredictClassifications> mockPredictor = new();
 
+    [Fact]
+    public void Verify_Returns_Bad_Request_When_Bad_Input_Received()
+    {
+        // Arrange
+        var sut = new Transactions(mockPredictor.Object, validator);
+        var transaction = TestData.CardTransaction();
+        transaction.Year = -1;
+
+        // Act
+        var response = sut.Verify(transaction);
+
+        // Assert
+        var result = Assert.IsType<BadRequestObjectResult>(response);
+        Assert.Contains("Invalid transaction submitted for verification", result.Value?.ToString());
+    }
+
+    [Fact]
+    public void Verify_Returns_Forbidden_When_Fraud_Predicted()
+    {
+        // Arrange
+        var sut = new Transactions(mockPredictor.Object, validator);
+        var transaction = TestData.CardTransaction();
+        mockPredictor.Setup(x => x.CheckIfFraud(transaction)).Returns(true);
+
+        // Act
+        var response = sut.Verify(transaction);
+
+        // Assert
+        var result = Assert.IsType<ObjectResult>(response);
+        Assert.Equal(403, result.StatusCode);
+        Assert.Contains("Transaction rejected as possible fraud", result.Value?.ToString());
+    }
+
+    [Fact]
+    public void Verify_Returns_Ok_When_No_Fraud_Predicted()
+    {
+        // Arrange
+        var sut = new Transactions(mockPredictor.Object, validator);
+        var transaction = TestData.CardTransaction();
+        mockPredictor.Setup(x => x.CheckIfFraud(transaction)).Returns(false);
+
+        // Act
+        var response = sut.Verify(transaction);
+
+        // Assert
+        var result = Assert.IsType<OkResult>(response);
     }
 }
--- credit-card-transactions/ml-net-example/ML.NET.Example/ML.NET.API.Tests/Services/PredictionServiceTests.cs
+++ credit-card-transactions/ml-net-example/ML.NET.Example/ML.NET.API.Tests/Services/PredictionServiceTests.cs
@@ -1,14 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ML.NET.API.Models;
+using ML.NET.API.Services;
 
 namespace ML.NET.API.Tests.Services;
 
 public class PredictionServiceTests
 {
-    [Fact]
-    public void Test1()
-    {
+    private readonly PredictionService predictionService = new();
 
+    [Fact]
+    public void Should_Return_True_For_Is_Fraud()
+    {
+        // Arrange
+        var transaction = new CardTransaction
+        {
+            User = 0,
+            Card = 0,
+            Amount = 287.13m,
+            Year = 2015,
+            Month = 11,
+            Day = 15,
+            Time = TimeOnly.Parse("12:15"),
+            UseChip = "Online Transaction",
+            MerchantName = "-8194607650924472520",
+            MerchantCity = "ONLINE",
+            MerchantState = double.NaN.ToString(),
+            Zip = double.NaN,
+            MCC = 3001
+        };
+
+        // Act
+        var result = predictionService.CheckIfFraud(transaction);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void Should_Return_False_For_Is_Fraud()
+    {
+        // Arrange
+        var transaction = new CardTransaction
+        {
+            User = 0,
+            Card = 0,
+            Amount = 287.13m,
+            Year = 2015,
+            Month = 11,
+            Day = 15,
+            Time = TimeOnly.Parse("12:15"),
+            UseChip = "Swipe Transaction",
+            MerchantName = "-8194607650924472520",
+            MerchantCity = "NYC",
+            MerchantState = "New York",
+            Zip = double.NaN,
+            MCC = 3001
+        };
+
+        // Act
+        var result = predictionService.CheckIfFraud(transaction);
+
+        // Assert
+        Assert.False(result);
     }
 }
